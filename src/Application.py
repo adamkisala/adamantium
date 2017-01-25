137acfa187e6888f8519186f87854dba2e85db1f
adam <adam.kisala@gmail.com>
2017-01-25 23:35:57 +0000

    Basic usage and error handling

--- src/Application.py
+++ src/Application.py
@@ -2,22 +2,44 @@ import logging
 import logging.config
 
 from controllers.LoggingController import LoggingController
+from exception.ExceptionHandler import ExceptionHandler
 from helpers.Constants import *
 from factory.GameFactory import *
 import controllers.GameController
 import sys
+from flask import Flask, Response, jsonify
+
+app = Flask(__name__)
+game_controller = None
+
+
+@app.errorhandler(ExceptionHandler)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route("/")
+def receive():
+    if game_controller is None:
+        raise ExceptionHandler("GAME_INSTANCE_NOT_RUNNING", 500)
+    try:
+        game_controller.play()
+    except Exception:
+        raise ExceptionHandler("UUUPS", 500)
 
 
 def main(argv):
     if len(sys.argv) > 0:
-
         # logger.basicConfig(filename='data.log', filemode='w', level=logging.DEBUG)
         file = FileStream(argv[1])
         game_fac = GameFactory()
         game = game_fac.create_game(file)
+        global game_controller
         game_controller = controllers.GameController.GameController(game)
-        game_controller.play()
-
+        # game_controller.play()
+        app.run()
     else:
         print(NO_GAME_DESCRIPTION)
 
